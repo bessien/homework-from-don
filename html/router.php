3ad4ae6da7bdfca570e99147f74dfe1969bc6069
bessien <barbaraessien@gmail.com>
2016-12-19 23:19:15 -0800

    toggle for log in/log out
    using separate files

--- html/router.php
+++ html/router.php
@@ -1,17 +1,19 @@
 <?php
+  if(isset($_REQUEST['color']))
+  include('./log_out.php');
+	else
+	include('./log_in.php');
+?>
+
+<?php
 	$page = $_SERVER["REQUEST_URI"];
 	$file = substr($page, 10);
 		if ($file == "/about")
 		include('./about.php');
 		elseif ($file == "/contact")
 		include('./contact.php');
-		elseif ($file == '/form') {
-			include('./form.php');
-			if (!empty($_REQUEST['color'])) {
-				$input_color = $_REQUEST['color'];
-				include('./loggedin.php');
-			}
-		}
+		elseif ($file == '/form')
+		include('./form.php');
 		else
 		echo "Welcome home.";
 ?>
